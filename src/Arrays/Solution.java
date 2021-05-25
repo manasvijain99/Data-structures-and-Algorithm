31c8ba0382e58626e1d9bd9d1745a6176d2f53e0
manasvijain99 <jain.manasvi13@gmail.com>
2021-05-25 14:44:09 +0530

    Array-Easy-Running Sum of 1d Array

---
+++ src/Arrays/Solution.java
@@ -0,0 +1,37 @@
+//******************** Running Sum of 1d Array *********************
+//Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+//
+//Return the running sum of nums.
+
+//Example 1:
+//
+//Input: nums = [1,2,3,4]
+//Output: [1,3,6,10]
+//Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+package Arrays;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public int[] runningSum(int[] nums) {
+        int  result[]=new int[nums.length];
+        result[0]=nums[0];
+        for(int i=1;i<nums.length;i++){
+            result[i]=nums[i]+result[i-1];
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums ={1,2,3,4};
+        Solution obj=new Solution();
+        int[] ans=obj.runningSum(nums);
+
+
+        System.out.println(Arrays.toString(ans));
+
+
+    }
+}
